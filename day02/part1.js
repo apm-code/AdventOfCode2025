a16e19d102d9b73c2eb273a98fe468a12103bb11
apm-code <adrianignacio.perez832@comunidadunir.net>
2025-12-03 20:18:35 +0100

    Add: Day02 (Uncompleted)

---
+++ day02/part1.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+
+// Leemos el input del día
+const input = fs.readFileSync(__dirname + '/input.txt', 'utf8').trim()
+const lines = input.split('\n')
+
+function solve() {
+  // TODO: implementa la lógica de la parte 1
+  let result = 0
+
+  // Ejemplo:
+  // for (const line of lines) {
+  //   // procesa cada línea
+  // }
+
+  return result
+}
+
+console.log('Resultado parte 1:', solve())
